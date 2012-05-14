e0409cc506fcfd12d1d3f1cb100dad495b020ca9
Sam <samtreweek@gmail.com>
2012-05-14 20:37:28 +0100

    all looks the same with a worse restructure...

--- app/overrides/restructure_cart_footer_section.rb
+++ app/overrides/restructure_cart_footer_section.rb
@@ -1,4 +1,9 @@
 Deface::Override.new(:virtual_path => "spree/orders/edit",
+                     :name => "advanced_cart_buttons",
+                     :insert_before => "[data-hook='cart_buttons']",
+                     :partial => "spree/orders/apply_coupon")
+
+Deface::Override.new(:virtual_path => "spree/orders/edit",
                      :name => "remove_cart_buttons",
                      :remove => "[data-hook='cart_buttons']")
 
